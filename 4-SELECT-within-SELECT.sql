373232506060990c56b92c169e6bcaf16f2aa294
Anil Kumar Poda <anilkumarpoda@gmail.com>
2023-03-10 19:09:49 +0000

    Solutions to SQLZoo Select within Select Quiz

---
+++ 4-SELECT-within-SELECT.sql
@@ -0,0 +1,14 @@
+/* Solutions to SQLZoo Select within Select quiz */
+
+--#1
+
+/* List each country name where the population is larger than that of 'Russia'.
+world(name, continent, area, population, gdp) */
+
+SELECT name
+FROM world
+WHERE population > (
+		SELECT population
+		FROM world
+		WHERE name = 'Russia'
+		)
