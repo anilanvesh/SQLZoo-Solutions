c3495b2f3038ae80af689028e9c6f2837b510c61
Anil Kumar Poda <anilkumarpoda@gmail.com>
2023-03-11 17:22:23 +0000

    Added description

--- 2-SELECT-from-WORLD.sql
+++ 2-SELECT-from-WORLD.sql
@@ -1,7 +1,8 @@
 /* SQLZoo SELECT from World Quiz with Answers */
 
 --#1
-/* Observe the result of running this SQL command to show the name, continent and population of all countries. */
+/* Introduction
+Observe the result of running this SQL command to show the name, continent and population of all countries. */
 
 SELECT name,
 	continent,
@@ -9,7 +10,8 @@ SELECT name,
 FROM world
 
 --#2
-/* How to use WHERE to filter records. 
+/* Large Countries
+How to use WHERE to filter records. 
 Show the name for the countries that have a population of at least 200 million. 
 200 million is 200000000, there are eight zeros. */
 
@@ -18,7 +20,8 @@ FROM world
 WHERE population >= 200000000
 
 --#3
-/* Give the name and the per capita GDP for those countries with a population of at least 200 million. */
+/* Per capita GDP
+Give the name and the per capita GDP for those countries with a population of at least 200 million. */
 
 SELECT name,
 	gdp / population AS 'per capita GDP'
@@ -26,7 +29,8 @@ FROM world
 WHERE population >= 200000000
 
 --#4
-/* Show the name and population in millions for the countries of the continent 'South America'. 
+/* South America In millions
+Show the name and population in millions for the countries of the continent 'South America'. 
 Divide the population by 1000000 to get population in millions. */
 
 SELECT name,
@@ -34,8 +38,9 @@ SELECT name,
 FROM world
 WHERE continent = 'South America'
 
---#5
-/* Show the name and population for France, Germany, Italy */
+--#5 
+/* France, Germany, Italy
+Show the name and population for France, Germany, Italy */
 
 SELECT name,
 	population
@@ -43,14 +48,16 @@ FROM world
 WHERE name IN ('France', 'Germany', 'Italy')
 
 --#6
-/* Show the countries which have a name that includes the word 'United' */
+/* United
+Show the countries which have a name that includes the word 'United' */
 
 SELECT name
 FROM world
 WHERE name LIKE '%United%'
 
 --#7
-/* Two ways to be big: A country is big if it has an area of more than 3 million sq km 
+/* Two ways to be big
+Two ways to be big: A country is big if it has an area of more than 3 million sq km 
 or it has a population of more than 250 million. 
 Show the countries that are big by area or big by population. 
 Show name, population and area. */
@@ -63,7 +70,8 @@ WHERE area > 3000000
 	OR population > 250000000
 
 --#8
-/* Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) 
+/* One or the other (but not both)
+Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) 
 or big by population (more than 250 million) but not both. 
 Show name, population and area.
 
@@ -86,7 +94,8 @@ WHERE (
 		)
 
 --#9
-/* Show the name and population in millions and the GDP in billions 
+/* Rounding
+Show the name and population in millions and the GDP in billions 
 for the countries of the continent 'South America'. 
 Use the ROUND function to show the values to two decimal places. 
 
@@ -99,7 +108,8 @@ FROM world
 WHERE continent = 'South America'
 
 --#10
-/* Show the name and per-capita GDP for those countries with a GDP of at least 
+/* Trillion dollar economies
+Show the name and per-capita GDP for those countries with a GDP of at least 
 one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000. */
 
 SELECT name,
@@ -108,7 +118,8 @@ FROM world
 WHERE gdp > 1000000000000
 
 --#11
-/* Greece has capital Athens.
+/* Name and capital have the same length
+Greece has capital Athens.
 Each of the strings 'Greece', and 'Athens' has 6 characters.
 Show the name and capital where the name and the capital have the same number of characters.
 
